3ff9a76a10af61ff7dc3206d582edeeb5af55633
Char <42180652+CharKeaney@users.noreply.github.com>
2021-01-24 22:21:19 +0000

    New test cases for wip features
    Some new features needed in parser.c to support them

--- lib/driver.c
+++ lib/driver.c
@@ -82,6 +82,51 @@ char* test5 =
   ret i32 0\n\
 }"; 
 
+char* test7 =
+"define i32 @main() #0 {\n\
+  %1 = alloca i8*, align 8\n\
+  %2 = alloca i8*, align 8\n\
+  %3 = alloca i64, align 8\n\
+  %4 = alloca i8*, align 8\n\
+  store i8* getelementptr inbounds ([6 x i8], [6 x i8]* @.str, i32 0, i32 0), i8** %1, align 8\n\
+  store i8* getelementptr inbounds ([9 x i8], [9 x i8]* @.str.1, i32 0, i32 0), i8** %2, align 8\n\
+  ret i32 0\n\
+}";/*
+  %5 = load i8*, i8** %1, align 8\n\
+  %6 = call i64 @strlen(i8* %5)\n\
+  %7 = add i64 %6, 1\n\
+  store i64 %7, i64* %3, align 8\n\
+  %8 = load i64, i64* %3, align 8\n\
+  %9 = call i8* @malloc(i64 %8)\n\
+  store i8* %9, i8** %4, align 8\n\
+  %10 = load i8*, i8** %4, align 8\n\
+  %11 = load i8*, i8** %1, align 8\n\
+  %12 = load i64, i64* %3, align 8\n\
+  %13 = load i8*, i8** %4, align 8\n\
+  %14 = call i64 @llvm.objectsize.i64.p0i8(i8* %13, i1 false)\n\
+  %15 = call i8* @__strncat_chk(i8* %10, i8* %11, i64 %12, i64 %14) #4\n\
+  %16 = load i8*, i8** %2, align 8\n\
+  %17 = call i64 @strlen(i8* %16)\n\
+  %18 = load i64, i64* %3, align 8\n\
+  %19 = add i64 %18, %17\n\
+  store i64 %19, i64* %3, align 8\n\
+  %20 = load i8*, i8** %4, align 8\n\
+  %21 = load i64, i64* %3, align 8\n\
+  %22 = call i8* @realloc(i8* %20, i64 %21)\n\
+  store i8* %22, i8** %4, align 8\n\
+  %23 = load i8*, i8** %4, align 8\n\
+  %24 = load i8*, i8** %2, align 8\n\
+  %25 = load i64, i64* %3, align 8\n\
+  %26 = load i8*, i8** %4, align 8\n\
+  %27 = call i64 @llvm.objectsize.i64.p0i8(i8* %26, i1 false)\n\
+  %28 = call i8* @__strncat_chk(i8* %23, i8* %24, i64 %25, i64 %27) #4\n\
+  %29 = load i8*, i8** %4, align 8\n\
+  %30 = call i32 @puts(i8* %29)\n\
+  %31 = load i8*, i8** %4, align 8\n\
+  call void @free(i8* %31)\n\
+  ret i32 0\n\
+}";*/
+
 void main(int argc, char* argv[]) {
 	/*
 		The job of main here is to act as a pipeline
@@ -93,7 +138,7 @@ void main(int argc, char* argv[]) {
 			test_tokenize();
 		}
 		char* words[PREPROCESSOR_BUFFER_SIZE];
-		preprocessor(test5, words);
+		preprocessor(test7, words);
 		token* tokens[TOKENS_BUFFER_SIZE];
 		tokenizer(words, tokens);
 		//print_token_list(tokens);
