25b6e363d265aecc0b5e0e71c96f3f04f85c8232
Char <42180652+CharKeaney@users.noreply.github.com>
2021-01-29 21:26:10 +0000

    Added new testcases
    Final test case not fully finished

--- lib/driver.c
+++ lib/driver.c
@@ -1,11 +1,9 @@
+﻿
 #include "parser.h"
 
+#define RUN_TESTS false
+
 char* test1 =
-"%c = fmul i32 %a, %b";
-
-char* test2 = "%3 = load i32 * %a.addr, align 4";
-
-char* test3 =
 "define i32 @sum(i32 %a, i32 %b) {\n\
 entry:\n\
 	 %c = fmul i32 %a, %b\n\
@@ -13,7 +11,7 @@ entry:\n\
 	ret i32 %add\n\
 }";
 
-char* test4 =
+char* test2 =
 "define i32 @sum(i32 %a, i32 %b) #0 {\n\
 entry:\n\
 	%a.addr = alloca i32, align 4\n\
@@ -26,7 +24,7 @@ entry:\n\
 	ret i32 %add\n\
 }";
 
-char* test5 =
+char* test3 =
 "define i32 @main() #0 {\n\
  %1 = alloca i32, align 4\n\
   %2 = alloca i32, align 4\n\
@@ -82,7 +80,34 @@ char* test5 =
   ret i32 0\n\
 }"; 
 
-char* test7 =
+char* test4 =
+"define internal void @__crystal_main(i32 %argc, i8** %argv) {\n\
+alloca:\n\
+  %num = alloca i32\n\
+  br label %entry\n\
+\n\
+entry:                                            ; preds = %alloca\n\
+  store i32 %argc, i32* @ARGC_UNSAFE\n\
+  store i8** %argv, i8*** @ARGV_UNSAFE\n\
+  store i32 0, i32* %num\n\
+  br label %while\n\
+\n\
+while:                                            ; preds = %body, %entry\n\
+  %0 = load i32, i32* %num\n\
+  %1 = icmp slt i32 %0, 5\n\
+  br i1 %1, label %body, label %exit\n\
+\n\
+body:                                             ; preds = %while\n\
+  %2 = load i32, i32* %num\n\
+  %3 = add i32 %2, 1\n\
+  store i32 %3, i32* %num\n\
+  br label %while\n\
+\n\
+exit:                                             ; preds = %while\n\
+  ret void\n\
+}";
+
+char* test5 =
 "define i32 @main() #0 {\n\
   %1 = alloca i8*, align 8\n\
   %2 = alloca i8*, align 8\n\
@@ -125,6 +150,86 @@ char* test7 =
   ret i32 0\n\
 }";
 
+test6 = 
+"define internal %String* @__crystal_main(i32 %argc, i8** %argv) {\n\
+alloca:\n\
+  %test = alloca %TestClass*\n\
+  %output = alloca %String*\n\
+  br label %entry\n\
+\n\
+entry:                                            ; preds = %alloca\n\
+  store i32 %argc, i32* @ARGC_UNSAFE\n\
+  store i8** %argv, i8*** @ARGV_UNSAFE\n\
+  %0 = call %TestClass* @\"*TestClass::new<String>:TestClass\"(%String* bitcast ({ i32, i32, i32, [5 x i8] }* @\"\'Demo\'\" to %String*))\n\
+  store %TestClass* %0, %TestClass** %test\n\
+  %1 = load %TestClass*, %TestClass** %test\n\
+  store %String* bitcast ({ i32, i32, i32, [20 x i8] }* @\"\'This is a TestClass...\'\" to %String*), %String** %output\n\
+  ret %String* bitcast ({ i32, i32, i32, [20 x i8] }* @\"\'This is a TestClass...\'\" to %String*)\n\
+}";
+
+/*
+declare i32 @printf(i8*, ...)
+
+; Function Attrs: uwtable
+define i32 @main(i32 %argc, i8** %argv) #0 {
+entry:
+  %0 = call %String* @__crystal_main(i32 %argc, i8** %argv)
+  ret i32 0
+}
+
+; Function Attrs: uwtable
+define internal %TestClass* @"*TestClass::new<String>:TestClass"(%String* %name) #0 {
+alloca:
+  %_ = alloca %TestClass*
+  br label %entry
+
+entry:                                            ; preds = %alloca
+  %malloccall = tail call i8* @malloc(i32 ptrtoint (%TestClass* getelementptr (%TestClass, %TestClass* null, i32 1) to i32))
+  %0 = bitcast i8* %malloccall to %TestClass*
+  %1 = bitcast %TestClass* %0 to i8*
+  call void @llvm.memset.p0i8.i32(i8* %1, i8 0, i32 ptrtoint (%TestClass* getelementptr (%TestClass, %TestClass* null, i32 1) to i32), i32 4, i1 false)
+  %2 = getelementptr inbounds %TestClass, %TestClass* %0, i32 0, i32 0
+  store i32 6, i32* %2
+  store %TestClass* %0, %TestClass** %_
+  %3 = load %TestClass*, %TestClass** %_
+  %4 = call %String* @"*TestClass#initialize<String>:String"(%TestClass* %3, %String* %name)
+  %5 = load %TestClass*, %TestClass** %_\n\
+  ret %TestClass* %5\n\
+}";*/
+
+
+
+bool test_passed(char* test) {
+	char* words[PREPROCESSOR_BUFFER_SIZE];
+	preprocessor(test, words);
+	token* tokens[TOKENS_BUFFER_SIZE];
+	tokenizer(words, tokens);
+	
+	int len = 0;
+	for (; tokens[len] != NULL; len++);
+
+	ast_node* root = match_module(tokens);
+	return root->size == len;
+}
+
+void run_test(char* test, int test_num) {
+	printf("\t[%c] Test %d %s.\n",
+		(test_passed(test)) ? 'Y' : 'N',
+		test_num,
+		(test_passed(test)) ? "Passed" : "Failed");
+}
+
+void run_tests(char** tests[]) {
+	for (int i = 0; tests[i] != NULL; i++) {
+		run_test(*tests[i], i + 1);
+	}
+}
+
+char **tests[] = {
+	&test1, &test2, &test3, &test4, &test5, //&test6
+};
+
+
 void main(int argc, char* argv[]) {
 	/*
 		The job of main here is to act as a pipeline
@@ -135,12 +240,13 @@ void main(int argc, char* argv[]) {
 		if (DEBUG_TEST_TOKENIZER) {
 			test_tokenize();
 		}
-		char* words[PREPROCESSOR_BUFFER_SIZE];
-		preprocessor(test7, words);
-		token* tokens[TOKENS_BUFFER_SIZE];
-		tokenizer(words, tokens);
-		print_token_list(tokens);
-		ast_node* root = match_module(tokens);
-		print_ast(root);
+
+		if (RUN_TESTS) {
+			printf("Performing tests...\n");
+			run_tests(tests);
+		}
+
+		run_test(test6, 6);
+
 	}
 }
